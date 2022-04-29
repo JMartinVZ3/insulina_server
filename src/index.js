4953d1894d3234018a4e996995bdc0362fbf7269
Martin Veliz <54328819+JMartinVZ3@users.noreply.github.com>
2022-04-29 16:18:55 -0500

    <feat>{added user auth and chat bot now working}

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// DB Config
+require('./database/config').dbConnection();
+
+
 // App de Express
 const app = express();
 
@@ -13,6 +17,8 @@ const server = require('http').createServer(app);
 
 
 // Mis Rutas
+app.use('/api/v1/login', require('./routes/auth'));
+app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/bot', require('./routes/bot'));
 
 server.listen(process.env.PORT, (err) => {
---
+++ src/routes/auth.js
@@ -0,0 +1,29 @@
+/*
+    path: api/v1/login
+
+*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { crearUser, login, renewToken} = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.post('/new', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check('email', 'El correo es obligatorio').isEmail(),
+    validarCampos
+], crearUser);
+
+
+router.post('/', [
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check('email', 'El correo es obligatorio').isEmail(),
+], login);
+
+router.get('/renew', validarJWT, renewToken);
+
+module.exports = router;
